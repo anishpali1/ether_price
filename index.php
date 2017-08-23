3e2bf4681af2782271fdf63a8a8c36bbb0ef777f
anish <anishpali1@gmail.com>
2017-08-23 15:23:20 +0530

    test

--- index.php
+++ index.php
@@ -1,38 +1,3 @@
 <?php
-    // this line loads the library 
-    require_once('Twilio.php'); 
-    
-    $account_sid = 'AC6ffa8c3b35decdcab1987621b7dd5d67';  
-    $auth_token = 'edb22a903581f1a4eb4ceb83ddf51c3d'; 
-    $from_num = "+19473339675";
-    $to_num="+919544818558";
-  
-    
-    $result=file_get_contents('https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD,INR');
-    
-    $response=json_decode($result,true); 
-    $message="1 Ether = $".$response['USD'].' / Rs.'.$response['INR'] ;  
-    
-    
-    
-    $client = new Services_Twilio($account_sid, $auth_token); 
-    try { 
-    $message = $client->account->messages->create(array( 
-        'To' => $to_num, 
-        'From' => $from_num,         
-        'Body' => $message,   
-    ));    
-    }    
-    catch (Services_Twilio_RestException $e) {
-   
-}
-
-//    if($message)
-//    {
-//        return "success msg sent";
-//    }
-//    else
-//    {
-//        return "error msg sending";
-//    }
+    echo "dfg";
 ?>
